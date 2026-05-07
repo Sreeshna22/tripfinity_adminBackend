582a6e541e13f03d09db45f1fe03f74b6e9b8f86
Sreeshna R <Sreeshnar1999@gmail.com>
2026-05-07 13:31:22 +0530

    added tripfinity admin panel

--- tripfinity-admin-backend/controllers/AuthController.js
+++ tripfinity-admin-backend/controllers/AuthController.js
@@ -164,4 +164,17 @@ authCtrl.getAdminProfile = async (req, res) => {
   }
 };
 
+
+authCtrl.Register = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    const hashed = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashed, role });
+    await newUser.save();
+    res.json({ msg: "Admin Created Successfully!" });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
+
 module.exports = authCtrl;
--- tripfinity-admin-backend/routes/authRoutes.js
+++ tripfinity-admin-backend/routes/authRoutes.js
@@ -2,21 +2,6 @@
  
 
 
-// const express = require("express");
-// const authRouter = express.Router();
-
-// const authCtrl = require("../controllers/AuthController");
-// const { authMiddleware, adminChecker } = require("../middleware/tokenMiddlewares");
-
-
-
-// authRouter.post("/admin-login", authCtrl.AdminLogin);
-// authRouter.post("/forgot-pwd/send-otp", authCtrl.sendOTPForgotPwd);
-// authRouter.post("/forgot-pwd/verify-otp", authCtrl.verifyOTPForgotPwd);
-// authRouter.post("/change-pwd", authCtrl.changePwd);
-// authRouter.get("/admin-profile", authMiddleware, adminChecker, authCtrl.getAdminProfile);
-
-// module.exports = authRouter;
 
 
 const express = require("express");
@@ -31,6 +16,8 @@ authRouter.post(
     authCtrl.loginLimiter, 
     authCtrl.AdminLogin
 );
+
+authRouter.post("/register", authCtrl.Register);
 
 
 authRouter.post("/forgot-pwd/send-otp", authCtrl.sendOTPForgotPwd);
